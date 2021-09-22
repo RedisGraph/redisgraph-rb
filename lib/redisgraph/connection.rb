04a1f878355ebd509e2977a6099003741c531ebe
Jeffrey Lovitz <jeffrey.lovitz@gmail.com>
2021-09-22 12:47:33 -0400

    Skip module check if server doesn't support MODULE LIST

--- lib/redisgraph/connection.rb
+++ lib/redisgraph/connection.rb
@@ -1,19 +1,26 @@
 class RedisGraph
   def connect_to_server(options)
     @connection = Redis.new(options)
-    @module_version = module_version()
-    raise ServerError, "RedisGraph module not loaded." if @module_version.nil?
-    raise ServerError, "RedisGraph module incompatible, expecting >= 1.99." if @module_version < 19900
+    check_module_version
   end
 
   # Ensure that the connected Redis server supports modules
   # and has loaded the RedisGraph module
-  def module_version()
+  def check_module_version()
     redis_version = @connection.info["redis_version"]
     major_version = redis_version.split('.').first.to_i
     raise ServerError, "Redis 4.0 or greater required for RedisGraph support." unless major_version >= 4
-    modules = @connection.call("MODULE", "LIST")
+
+    begin
+      modules = @connection.call("MODULE", "LIST")
+    rescue Redis::CommandError
+      # Ignore check if the connected server does not support the "MODULE LIST" command
+      return
+    end
+
     module_graph = modules.detect { |_name_key, name, _ver_key, _ver| name == 'graph' }
-    module_graph[3] if module_graph
+    module_version = module_graph[3] if module_graph
+    raise ServerError, "RedisGraph module not loaded." if module_version.nil?
+    raise ServerError, "RedisGraph module incompatible, expecting >= 1.99." if module_version < 19900
   end
 end
